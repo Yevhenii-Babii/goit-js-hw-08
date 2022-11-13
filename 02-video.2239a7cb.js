369ce6b3fc819f2ee57f403bcf5021cc83bc85f9
Yevhenii-Babii <113521371+Yevhenii-Babii@users.noreply.github.com>
2022-11-13 12:53:22 +0000

    Deploying to gh-pages from @ Yevhenii-Babii/goit-js-hw-08@8457045fd66d87962164db342841d551dd5072ce 🚀

--- 02-video.2239a7cb.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe");console.log(e);const t=new Vimeo.Player(e);t.on("timeupdate",(function({seconds:e}){localStorage.setItem("videoplayer-current-time",e)})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
-//# sourceMappingURL=02-video.2239a7cb.js.map
--- 02-video.d642876d.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe");console.log(e);var t=new Vimeo.Player(e);t.on("timeupdate",(function(e){var t=e.seconds;localStorage.setItem("videoplayer-current-time",t)})),t.setCurrentTime(localStorage.getItem("videoplayer-current-time"))}();
-//# sourceMappingURL=02-video.d642876d.js.map
