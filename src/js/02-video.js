5e359923e35e880bb5e0e5e495a7c095de7ebb4f
Evhenii Babii <113521371+Yevhenii-Babii@users.noreply.github.com>
2022-11-13 21:16:55 +0200

    add correct

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,7 +4,7 @@ var throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
 
-player.on(`timeupdate`,  throttle(onPlay, 300));
+player.on(`timeupdate`,  throttle(onPlay, 1000));
 
 function onPlay({seconds}) {
     localStorage.setItem('videoplayer-current-time', seconds);
@@ -12,7 +12,7 @@ function onPlay({seconds}) {
 }
 
 
-player.setCurrentTime(localStorage.getItem("videoplayer-current-time") ?? 0);
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time"));
 
 
 
