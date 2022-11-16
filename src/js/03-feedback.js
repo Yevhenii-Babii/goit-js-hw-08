a1e1e330548c1f643b87df3ab2cbfee4f5faba94
Evhenii Babii <113521371+Yevhenii-Babii@users.noreply.github.com>
2022-11-16 18:54:28 +0200

    add correction

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,5 +1,4 @@
 
-
 var throttle = require('lodash.throttle');
 
 const form = document.querySelector('.feedback-form');
@@ -7,36 +6,47 @@ const form = document.querySelector('.feedback-form');
 form.addEventListener('input', throttle(onFormData, 500));
 form.addEventListener('submit', onSubmitForm);
 
+let formData = {};
 
-var formData = {};
-onInputSettings();
 
 
 function onFormData(event) {
   const name = event.target.name
   const value = event.target.value;
-
-  formData[name] = value;
-
+  
+  formData = {
+    ...formData,
+    [name]: value,
+  }
+console.log(formData);
   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+  
 }
+
 
 function onSubmitForm(event) {
   JSON.parse(localStorage.getItem('feedback-form-state'));
   event.preventDefault();
   event.target.reset();
-  formData = {};
   localStorage.removeItem('feedback-form-state');
- 
+
+   formData = {}
 }
 
 function onInputSettings () {
-  const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state'));
+  const savedMessage = JSON.parse(localStorage.getItem('feedback-form-state') ?? {} );
+
   for (const prop in savedMessage ) {
     if(savedMessage.hasOwnProperty(prop)) {
       form.elements[prop].value = savedMessage[prop];
+      formData[prop] = savedMessage[prop]
     }
   }
-  formData = savedMessage;
+
 }
+
+onInputSettings();
+
+
+
 
